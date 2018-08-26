6b01e0eb144160c7211f4b784a5e948136e10ab0
Brian Rotich <23168146+brotich@users.noreply.github.com>
2018-08-26 04:15:49 +0300

    add elsint an config for vue bindings.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <h1>Welcome to Your Vue.js App</h1>
-  </div>
+  <v-ons-page>
+    <v-ons-toolbar>
+      <div class="center">{{ title }}</div>
+      <div class="right">
+        <v-ons-toolbar-button icon="ion-navicon, material: md-menu"></v-ons-toolbar-button>
+      </div>
+    </v-ons-toolbar>
+
+    <p style="text-align: center">
+      Some content.
+    </p>
+  </v-ons-page>
 </template>
+
+<style>
+  /* CSS goes here */
+</style>
+
+<script>
+  // Javascript goes here
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue';
+import VueOnsen from 'vue-onsenui';
+
 import App from './App.vue';
+
+
+Vue.use(VueOnsen);
 
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
+  data() {
+    return {
+      title: 'My app'
+    };
+  },
 }).$mount('#app');
